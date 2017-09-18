9070f84a968f7303bf5d809b357d6cb9deff0736
lemonCjlu <brith109@126.com>
2017-09-18 16:14:45 +0800

    2017-09-17 coding for mission 2.

--- mission2.py
+++ mission2.py
@@ -6,8 +6,34 @@ import string
 # print(ord('a'))
 ss = {ord(i):i for i in string.ascii_lowercase}
 
+s = '''g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr
+q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'''
+
 def encode(x):
-    return x + 2
+    if x not in string.ascii_lowercase:
+        return x
+    i = ord(x)
+    res = 0
+    if i in range(97,121):
+        res = i + 2
+    elif i == 121:
+        res = 97
+    elif i == 122:
+        res = 98
+    else:
+        pass
+    return chr(res)
+
+
+
+# a = ''.join(list(map(encode, list(s))))
+# print(a)
+#
+# raw_url = '''map'''
+# b = ''.join(list(map(encode, list(raw_url))))
+# print(b)
+
+
 
 
 
