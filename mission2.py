19bccc9b137472e5cb44ef7d37b5d82cfa8a0b54
lemonCjlu <brith109@126.com>
2017-09-19 21:15:00 +0800

    2017-09-17 coding for mission 2.

--- mission2.py
+++ mission2.py
@@ -1,13 +1,14 @@
 #! /user/bin/python
-
+'''
+Author:lemon
+Date:2017/9/19
+Method: translate string according to a letters into a letter, for example: abc into cde
+'''
 import string
 
 # string.ascii_lowercase
 # print(ord('a'))
-ss = {ord(i):i for i in string.ascii_lowercase}
-
-s = '''g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr
-q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'''
+#ss = {ord(i):i for i in string.ascii_lowercase}
 
 def encode(x):
     if x not in string.ascii_lowercase:
@@ -24,14 +25,22 @@ def encode(x):
         pass
     return chr(res)
 
+def translate(x):
+    if x not in string.ascii_lowercase:
+        return x
+    else:
+        return chr((ord(x)-97)%25+ 1 + 97)
 
 
+s = '''g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr
+q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'''
 # a = ''.join(list(map(encode, list(s))))
 # print(a)
 #
 # raw_url = '''map'''
 # b = ''.join(list(map(encode, list(raw_url))))
 # print(b)
+print(translate('y'))
 
 
 
