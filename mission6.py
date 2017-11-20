dec8333e3963c3d5249440871a73eeba430058ae
lemonCjlu <brith109@126.com>
2017-11-20 16:33:08 +0800

    2017-11-20 coding for mission 5(usage of pickle).

---
+++ mission6.py
@@ -0,0 +1,12 @@
+import pickle
+import urllib.request
+
+url = 'http://www.pythonchallenge.com/pc/def/channel.html'
+p_obj = pickle.load(urllib.request.urlopen("http://www.pythonchallenge.com/pc/def/banner.p"))
+print(p_obj)
+
+for i in p_obj:
+    line = ""
+    for content, num in i:
+        line = line + content*num
+    print(line)
