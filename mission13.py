d80c7c3894d3fe6404a7771a3a38469b7ff2e9bc
lemonCjlu <brith109@126.com>
2017-12-04 16:41:12 +0800

    2017-12-04 coding for mission 12(usage of file,切片，%格式化对字节进行读写). 1.该题需要在url中将.jpg改成gfx,而不是先下载图片再改后缀名，如果这样做，数据会发生改变； 2.读取字节数据并切片获取相应数据，再进行书写； 3.因为是对文件进行字节操作，所以指定模式应为rb/wb，且可以使用with关键字

---
+++ mission13.py
@@ -0,0 +1,13 @@
+from PIL import Image
+url = 'http://www.pythonchallenge.com/pc/return/disproportional.html'
+
+with open('evil2.gfx', 'rb') as f:
+    data = f.read()
+print(len(data))
+
+for i in range(5):
+   with open('image%d.jpg'%i, 'wb') as f:   #%的用法
+       f.write(data[i::5])                      #通过切片获取数据并进行写
+
+
+
